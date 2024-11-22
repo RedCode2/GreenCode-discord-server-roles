c6931a63aa0a91598448dc934942bfbcf422bd8a
Rounak Karmakar <rounakk2011@gmail.com>
2024-11-22 22:51:30 +0530

    Important changes

---
+++ static/scripts/homepage.scripts/homepage-script.js
@@ -0,0 +1,24 @@
+document.getElementById('memelord__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/memelord.html', '_parent');
+})
+document.getElementById('patreon-donor__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/patreon-donor.html', '_parent');
+})
+document.getElementById('server-booster__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/server-booster.html', '_parent');
+})
+document.getElementById('to-focus__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/to-focus.html', '_parent');
+});
+document.getElementById('green-coder__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/green-coder.html', '_parent');
+});
+document.getElementById('question-of-the-day__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/question-of-the-day.html', '_parent');
+});
+document.getElementById('other-roles__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/other-roles.html', '_parent');
+});
+document.getElementById('professional__role').addEventListener('click', function() {
+    window.open('static/webpages/roles/professional.html', '_parent');
+});
